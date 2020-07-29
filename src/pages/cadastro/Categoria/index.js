46333b0d5b255a40e8e0f2869390a70f67986d42
diegovss <diego@Diegos-MacBook-Pro.local>
2020-07-29 16:20:32 -0300

    teste

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -5,11 +5,24 @@ import { Link } from 'react-router-dom'
 
 function CadastroCategoria() {
     return(
-      <PageDefault>
-        <h1>
-          Castro de Categoria 
-        </h1>
+        <PageDefault>
+        <h1>Cadastro de Categoria</h1>
 
+        <form>
+  
+          <label>
+            Nome da Categoria:
+            <input
+              type="text"
+            />
+          </label>
+  
+          <button>
+            Cadastrar
+          </button>
+        </form>
+  
+  
         <Link to="/">
           Ir para home
         </Link>
